e81e352b3abf99fcc7da29bc1b5ab378410ef64f
Jacky Tang <jackytang74@gmail.com>
2020-03-11 17:39:21 -0700

    webpack is configured and jest seems to be ready, enzyme will be next

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>dlroW olleH</div>);
+  }
+}
+
+export default App;
