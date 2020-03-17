a5515664a6050b9f138c78365cb665c2d654fc04
Jacky Tang <jackytang74@gmail.com>
2020-03-17 11:47:08 -0700

    beginning react skeleton and more test ideas

---
+++ client/src/EntrySlider.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class EntrySlider extends React.Component {
+  // will contain the miniature slider for each listing
+  // initially will be unused and static images while I work on slider functionality
+  // can use big slider logic here in small slider
+}
+
+export default EntrySlider;
